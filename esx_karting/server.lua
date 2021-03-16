886a64fe2009087f8765b14a963097fe85435275
zNyte <79381762+zNyte@users.noreply.github.com>
2021-03-16 16:38:54 +0100

    Updated server.lua
    added money and item check, now the script isn't vulnerable!

--- esx_karting/server.lua
+++ esx_karting/server.lua
@@ -7,20 +7,37 @@ RegisterServerEvent('hotdogs')
 AddEventHandler('hotdogs', function()
     _source = source
     xPlayer = ESX.GetPlayerFromId(_source)
-
-    xPlayer.removeMoney(200)
-    xPlayer.addInventoryItem('hotdog', 1)
-    TriggerClientEvent('mythic_notify:client:SendAlert', _source, {type = 'error', text = ('Hai comprato un hotdog!'), length = 5500})
-
+    hotdog = xPlayer.getInventoryItem('hotdog')
+        
+    if hotdog.limit ~= -1 and (hotdog.count + 1) > hotdog.limit then
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, {type = 'error', text = ('Inventory Full'), length = 5500})
+	else
+        if xPlayer.getMoney() >= 200 then
+			xPlayer.removeMoney(200)
+			xPlayer.addInventoryItem("hotdog", 1)
+			TriggerClientEvent('mythic_notify:client:SendAlert', _source, {type = 'error', text = ('Successfully Buyed!'), length = 5500})
+		    else			
+		    TriggerClientEvent('mythic_notify:client:SendAlert', _source, {type = 'error', text = ('You dont have enough money!'), length = 5500})
+	    end       
+	end 
 end)
+
 
 RegisterServerEvent('cocacola')
 AddEventHandler('cocacola', function()
     _source = source
     xPlayer = ESX.GetPlayerFromId(_source)
-
-    xPlayer.removeMoney(200)
-    xPlayer.addInventoryItem('cocacola', 1)
-    TriggerClientEvent('mythic_notify:client:SendAlert', _source, {type = 'error', text = ('Hai comprato una cocacola!'), length = 5500})
-
+    cocacola = xPlayer.getInventoryItem('cocacola')
+       
+    if cocacola.limit ~= -1 and (cocacola.count + 1) > cocacola.limit then
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, {type = 'error', text = ('Inventory Full'), length = 5500})
+	else
+        if xPlayer.getMoney() >= 200 then
+			xPlayer.removeMoney(200)
+			xPlayer.addInventoryItem("cocacola", 1)
+			TriggerClientEvent('mythic_notify:client:SendAlert', _source, {type = 'error', text = ('Successfully Buyed!'), length = 5500})
+		    else			
+		    TriggerClientEvent('mythic_notify:client:SendAlert', _source, {type = 'error', text = ('You dont have enough money!'), length = 5500})
+	    end       
+	end 
 end)
